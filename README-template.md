c543262700f4e22a37811379d07bf850f2594542
Praixx <praix!y@gmail.com>
2023-11-12 20:59:14 +0100

    first commit

---
+++ README-template.md
@@ -0,0 +1,66 @@
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+
+
+
+## Overview
+
+This is a project created using studio ghibli api. it conatins search options which you can you to search for your 
+favourite studio ghibli animations and also a filter option
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Search for animations
+- filter animations
+
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Studio ghibli api
+
+
+### What I learned
+
+working on this project, i worked exrensively using apis. i dived deeper into the world of asynchronous javascript
+amd working with promises. i also learnt better ways of understanding json files i want to work on and also read them better. i also worked using a little bit of data structure and algorithm fro the search and filter section.
+
+
+
+## Author
+
+- Github - [Olaoye Praise](https://github.com/Praixx)
+- Email - [Olaoye Praise](praix1y@gmail.com)
+- Twitter - [@Praixx_](https://www.twitter.com/Praixx_)
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,183 @@
+const mainElement = document.querySelector("main");
+const navLinks = document.querySelectorAll("#mainnav ul li a");
+
+
+let filmData;   
+let dataSet = "films";
+let url = "https://ghibliapi.vercel.app/films";
+
+
+async function getData(url){
+    const dataPromise = await fetch(url);
+    const data = await dataPromise.json();
+
+    if(dataSet == "films"){
+        mainElement.innerHTML = "";
+        setSort(data);
+        addCard(data);
+        filmData = data;
+        document.querySelector("nav form").style.visibility = "visible";
+        document.getElementById("sortorder").removeAttribute("disabled")
+    }else{
+        mainElement.innerHTML = "";
+        document.querySelector("nav form").style.visibility = "hidden";
+        addCard(data); 
+    }
+    
+    
+}
+
+getData(url);
+
+
+document.getElementById("sortorder").addEventListener("change", function(){
+    mainElement.innerHTML = "";
+    //getFilms()
+    //setSort(filmData);
+    addCard(filmData)
+})
+
+navLinks.forEach(function(eachLink){
+    eachLink.addEventListener("click", function(event){
+        event.preventDefault();
+        const thisLink = event.target.getAttribute("href").substring(1);
+        url = "https://ghibliapi.vercel.app/" + thisLink;
+        dataSet = thisLink
+        getData(url)
+    })
+})
+
+function setSort(array){
+    const sortOrder = document.getElementById("sortorder").value;
+
+    switch(sortOrder){
+        case "title": array.sort((a, b) => (a.title > b.title) ? 1 : -1 ); break;
+        case "release_date": array.sort((a, b) => (a.release_date > b.release_date) ? 1: -1); break;
+        case "rt_score": array.sort((a, b) => (parseInt(a.rt_score) > parseInt(b.rt_score)) ? -1 : 1); break;
+    }
+}
+
+function addCard (array){
+    array.forEach( eachItems =>{
+        createCard(eachItems);
+    })
+}
+
+async function createCard (data){
+    const card = document.createElement("article");
+    switch(dataSet){
+        case "films" : card.innerHTML = filmCardContent(data); break;
+        case "people": card.innerHTML = await peopleCardContent(data); break;
+        case "locations": card.innerHTML = await locationCardContent(data); break;
+        case "species": card.innerHTML = await speciesCardContent(data); break;
+        case "vehicles": card.innerHTML = await vehicleCardContent(data); break;
+    }
+    mainElement.appendChild(card);  
+}
+
+function filmCardContent (data){
+    let html = `<h2>${data.title}</h2>`;
+    html += `<p><strong>Director: ${data.director}</strong></p>`;
+    html += `<p><strong>Released: ${data.release_date} </strong></p>`;
+    html += `<p><strong>Rating: ${data.rt_score} </strong></p>`;
+    html += `<p>${data.description} </p>`;
+    return html;
+}
+
+async function peopleCardContent (data){
+
+    const theFilms = data.films;
+    let filmTitles = [];
+    for( eachFilm of theFilms){
+        const filmTitle = await individualItems(eachFilm, "title");
+        filmTitles.push(filmTitle); 
+    }
+
+    const species = await individualItems(data.species, "name");
+    let html = `<h2>${data.name} </h2>` ;
+    html += `<p><strong>Details:</strong> gender ${data.gender}, age ${data.age}, eye color ${data.eye_color}, 
+    hair color ${data.hair_color} </p>`;
+    html += `<p><strong>Films:</strong> ${filmTitles.join(", ")}</p>`
+    html += `<p><strong>Species:</strong> ${species} </p>`;
+    
+    return html
+}
+
+async function locationCardContent(data){
+    const regex = 'https?:\/\/'; 
+
+    const theResident = data.residents;
+    let residentNames = [];
+    for(eachResident of theResident){
+       if(eachResident.match(regex)){
+            const resName = await individualItems(eachResident, "name");
+            residentNames.push(resName);
+        }else{
+            residentNames[0] = "no data available";
+        }
+    }
+
+    const thefilms = data.films;
+    let filmtitles = [];
+    for(eachFilm of thefilms){
+        const filmTitle = await individualItems(eachFilm, "title");
+        filmtitles.push(filmTitle);
+
+    }
+
+    let html = `<h2>${data.name} </h2>`;
+    html += `<p><strong>Details:</strong> climate${data.climate}, terrain${data.terrain}, surface water ${data.surface_water}% </p>`;
+    html += `<p><strong>Residents:</strong> ${residentNames.join(', ')} </p>`;
+    html += `<p><strong>Films:</strong> ${filmtitles.join(', ')} </p>`;
+    return html
+}
+
+async function speciesCardContent(data){
+    const people = data.people;
+    let peopleNames = [];
+    for(eachperson of people ){
+        const personName = await individualItems(eachperson, "name");
+        peopleNames.push(personName)
+    }
+
+    const thefilms = data.films;
+    let filmtitles = [];
+    for(eachFilm of thefilms){
+        const filmTitle = await individualItems(eachFilm, "title");
+        filmtitles.push(filmTitle)
+    }
+
+    let html = `<h2>${data.name} </h2>`;
+    html += `<p><strong>Classificstion:</strong> ${data.classification}</p>`;
+    html += `<p><strong>Eye Colors:</strong> ${data.eye_colors}</p>`;
+    html += `<p><strong>Hair Colors:</strong> ${data.hair_colors}</p>`;
+    html += `<p><strong>People:</strong> ${peopleNames.join(', ')}</p>`;
+    html += `<p><strong>Films:</strong> ${filmtitles.join(', ')}</p>`;
+    return html
+}
+
+async function vehicleCardContent(data){
+    let html = `<h2>${data.name}</h2>`;
+    html += `<p><strong>Description:</strong> ${data.description}</p>`;
+    html += `<p><strong>Vehicle:</strong> ${data.vehicle_class}</p>`;
+    html += `<p><strong>Length:</strong> ${data.length}</p>`;
+    html += `<p><strong>Pilot:</strong> ${await individualItems(data.pilot, "name")}</p>`;
+    html += `<p><strong>Film:</strong> ${await individualItems(data.films, "title")}</p>`;
+    return html;
+}
+
+
+
+async function individualItems (url, item){
+    var theItem
+
+   try{
+        const itemPromise = await fetch(url);
+        const data =  await itemPromise.json();
+        theItem = data[item] 
+   }catch(err){
+    theItem = "no data available"
+   }finally{
+    return theItem
+   }
+}
